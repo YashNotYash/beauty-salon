2903ffc814daf5d12d304a65f155cad948706a19
YashNotYash <prajapatiyash265@gmail.com>
2025-08-05 18:04:49 -0400

    Routing issue fix.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
@@ -9,21 +9,19 @@ function App() {
 
   return (
     <>
-      <Router>
-        <nav className="navbar">
-          <div className="navbar-logo">Maa Beauty Care</div>
-          <div className="navbar-links">
-            <Link to="/" className="navbar-link">Home</Link>
-            <Link to="/services" className="navbar-link">Services</Link>
-            <Link to="/contact" className="navbar-link">Contact</Link>
-          </div>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <nav className="navbar">
+        <div className="navbar-logo">Maa Beauty Care</div>
+        <div className="navbar-links">
+          <Link to="/" className="navbar-link">Home</Link>
+          <Link to="/services" className="navbar-link">Services</Link>
+          <Link to="/contact" className="navbar-link">Contact</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
